1e3a5351d05b9b55c2dc86668c83ca99de62f8d6
James Kiesel <james.kiesel@gmail.com>
2015-05-24 14:28:24 +0700

    Add rudimentary Ember component

---
+++ assets/javascripts/discourse/models/babble.js.es6
@@ -0,0 +1,8 @@
+import RestModel from 'discourse/models/rest';
+import Topic from 'discourse/models/topic';
+import { throwAjaxError } from 'discourse/lib/ajax-error';
+
+const Babble = RestModel.extend({
+});
+
+export default Babble;
--- plugin.rb
+++ plugin.rb
@@ -4,8 +4,43 @@
 # authors: James Kiesel (gdpelican)
 # url: https://github.com/gdpelican/babble
 
-module ::Babble
-  class Engine < ::Rails::Engine
-    engine_name PLUGIN_NAME
+register_asset "stylesheets/babble_sidebar.css"
+
+PLUGIN_NAME ||= "babble".freeze
+BABBLE_TOPIC_ID ||= -1
+BABBLE_TOPIC_TITLE ||= "Title for Babble Topic"
+
+after_initialize do
+  module ::Babble
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace Babble
+    end
+  end
+
+  class ::Babble::Controller < ActionController::Base
+    def show
+      format.json do
+        render_json_dump(TopicViewSerializer.new(TopicView.new(Babble::Topic.find_or_create.id, system_guardian), 
+                                                 scope: system_guardian,
+                                                 root: false))
+      end
+    end
+
+    private
+
+    def system_guardian
+      @system_guardian ||= Guardian.new Discourse.system_user
+    end
+  end
+
+  class ::Babble::Topic
+    def self.find_or_create
+      Topic.unscoped.find_by(id: BABBLE_TOPIC_ID) ||
+      Topic.create!(id: BABBLE_TOPIC_ID, 
+                    user: Discourse.system_user,
+                    title: BABBLE_TOPIC_TITLE,
+                    deleted_at: Time.now)
+    end
   end
 end
