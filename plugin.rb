f60c1bcfb359e48bf0f615726fdd62b1b506c482
James Kiesel <james.kiesel@gmail.com>
2016-11-07 10:27:28 -0500

    Remove chat messages from user activity / summary

--- plugin.rb
+++ plugin.rb
@@ -532,4 +532,32 @@ after_initialize do
     end
     prepend DefaultResults
   end
+
+  class ::UserAction
+    module ApplyCommonFilters
+      def apply_common_filters(builder, user_id, guardian, ignore_private_messages=false)
+        builder.where("t.archetype <> :chat", chat: Archetype.chat)
+        super(builder, user_id, guardian, ignore_private_messages)
+      end
+    end
+    singleton_class.prepend ApplyCommonFilters
+  end
+
+  UserSummary.class_eval do # why do I have to do it this way?
+    module RemoveChatTopics
+      def topics
+        super.where('archetype <> ?', Archetype.chat)
+      end
+
+      def replies
+        super.where('topics.archetype <> ?', Archetype.chat)
+      end
+
+      def links
+        super.where('topics.archetype <> ?', Archetype.chat)
+      end
+    end
+    prepend RemoveChatTopics
+  end
+
 end
