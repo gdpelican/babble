52f80cd787e27ed4fad967f291ffbcba77b5db39
James Kiesel <james.kiesel@gmail.com>
2017-02-26 17:24:45 +1300

    Use lt/gt or equal to in post stream window

--- app/services/post_stream_window.rb
+++ app/services/post_stream_window.rb
@@ -1,6 +1,6 @@
 class PostStreamWindow
   def self.for(topic:, from: (topic.highest_post_number+1), order: :desc, limit: SiteSetting.babble_page_size)
-    lt_or_gt = (order.to_sym == :desc) ? '<' : '>'
+    lt_or_gt = (order.to_sym == :desc) ? '<=' : '>='
     topic.posts.includes(:user)
                .where("post_number #{lt_or_gt} ?", from)
                .order(post_number: order)
