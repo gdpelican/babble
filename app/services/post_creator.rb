9f827ccd6fac30832461025a97f66c185b7f5af2
ziptofaf <ziptofaf1@gmail.com>
2018-01-07 11:20:09 +0100

    Update post_creator.rb
    Seems that latest discourse update made it that trigger_after_events no longer accepts a post parameter, it uses @post now. At the very least it fixes a freezing issue on my end.

--- app/services/post_creator.rb
+++ app/services/post_creator.rb
@@ -24,12 +24,12 @@ class ::Babble::PostCreator < ::PostCreator
     false
   end
 
-  def trigger_after_events(post)
+  def trigger_after_events
     super
 
-    post.trigger_post_process(true)
+    @post.trigger_post_process(true)
     TopicUser.update_last_read(@user, @topic.id, @post.post_number, @post.post_number, PostTiming::MAX_READ_TIME_PER_BATCH)
-    PostAlerter.post_created(post)
+    PostAlerter.post_created(@post)
 
     Babble::Broadcaster.publish_to_posts(@post, @user)
     Babble::Broadcaster.publish_to_topic(@topic, @user)
