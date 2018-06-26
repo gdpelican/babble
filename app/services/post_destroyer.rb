aba8bfa5b9543b711fbe73a8acdd9f933cfeb5f3
James Kiesel <james.kiesel@gmail.com>
2018-06-26 13:39:07 -0500

    Don't update user counts on chat destroy

--- app/services/post_destroyer.rb
+++ app/services/post_destroyer.rb
@@ -4,4 +4,8 @@ class ::Babble::PostDestroyer < ::PostDestroyer
     Babble::Broadcaster.publish_to_topic(@topic, @user)
     Babble::Broadcaster.publish_to_posts(@post, @user, is_delete: true)
   end
+
+  def update_user_counts
+    # Don't update user counts on post destroyal
+  end
 end
